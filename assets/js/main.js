b401f0750e395508b5916ba670935d428e05b734
Luiz Monge <luizfilipemonge@outlook.com>
2022-05-11 20:45:11 -0300

    [Base] - Cópia de canvas

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,11 +20,21 @@ function drawImage() {
   ctxOgCanvas.clearRect(0, 0, ogCanvas.width, ogCanvas.height);
 
   imgVar.onload = function () {
+    var mRatio = Math.min(450 / imgVar.width, 450 / ogCanvas.height);
+
+    imgVar.width > 450
+      ? (ogCanvas.width = mRatio * imgVar.width)
+      : (ogCanvas.width = imgVar.width);
+
+    imgVar.height > 450
+      ? (ogCanvas.height = mRatio * imgVar.height)
+      : (ogCanvas.height = imgVar.height);
+
     //Calcular a proporção da imagem em relação ao Canvas para aplicar "Fit";
     var hRatio = ogCanvas.width / imgVar.width;
     var vRatio = ogCanvas.height / imgVar.height;
-    var mRatio = Math.min(hRatio, vRatio);
-    
+    mRatio = Math.min(hRatio, vRatio);
+
     //Calculo o centro para posicionar a imagem em relação a canvas;
     var centerX = (ogCanvas.width - imgVar.width * mRatio) / 2;
     var centerY = (ogCanvas.height - imgVar.height * mRatio) / 2;
@@ -38,4 +48,11 @@ function drawImage() {
       imgVar.height * mRatio
     );
   };
+}
+
+function copyImage(fromCanvas, toCanvas, toContext) {
+  toCanvas.width = fromCanvas.width;
+  toCanvas.height = fromCanvas.height;
+  toContext.clearRect(0, 0, toCanvas.width, toCanvas.height);
+  toContext.drawImage(fromCanvas, 0, 0);
 }
