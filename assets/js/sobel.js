eb998091a405b7893f32efa430d0d198841964c1
Luiz Monge <luizfilipemonge@outlook.com>
2022-05-18 22:00:25 -0300

    [Sobel] - Base

---
+++ assets/js/sobel.js
@@ -0,0 +1,61 @@
+const gradientX = [-1.0, 0.0, +1.0, -2.0, 0.0, +2.0, -1.0, 0.0, +1.0]; //Kernel X
+
+const gradientY = [-1.0, -2.0, -1.0, 0.0, 0.0, 0.0, +1.0, +2.0, +1.0]; //Kernel Y
+
+function applySobel(targetCanvas, targetCanvasCtx) {
+  toGrayScale(targetCanvas, targetCanvasCtx);
+
+  const imageData = targetCanvasCtx.getImageData(
+    0,
+    0,
+    targetCanvas.width,
+    targetCanvas.height
+  );
+
+  let pixels = [];
+
+  const getPixel = (data, width, x, y) => {
+    return data[(y * width + x) * 4];
+  }; //Retorna o pixel relativo a imagedata
+
+  for (let y = 0; y < imageData.height; y++) {
+    for (let x = 0; x < imageData.width; x++) {
+      let pX =
+        gradientX[0] * getPixel(imageData.data, imageData.width, x - 1, y - 1) +
+        gradientX[1] * getPixel(imageData.data, imageData.width, x, y - 1) +
+        gradientX[2] * getPixel(imageData.data, imageData.width, x + 1, y - 1) +
+        gradientX[3] * getPixel(imageData.data, imageData.width, x - 1, y) +
+        gradientX[4] * getPixel(imageData.data, imageData.width, x, y) +
+        gradientX[5] * getPixel(imageData.data, imageData.width, x + 1, y) +
+        gradientX[6] * getPixel(imageData.data, imageData.width, x - 1, y + 1) +
+        gradientX[7] * getPixel(imageData.data, imageData.width, x, y + 1) +
+        gradientX[8] * getPixel(imageData.data, imageData.width, x + 1, y + 1);
+
+      let pY =
+        gradientY[0] * getPixel(imageData.data, imageData.width, x - 1, y - 1) +
+        gradientY[1] * getPixel(imageData.data, imageData.width, x, y - 1) +
+        gradientY[2] * getPixel(imageData.data, imageData.width, x + 1, y - 1) +
+        gradientY[3] * getPixel(imageData.data, imageData.width, x - 1, y) +
+        gradientY[4] * getPixel(imageData.data, imageData.width, x, y) +
+        gradientY[5] * getPixel(imageData.data, imageData.width, x + 1, y) +
+        gradientY[6] * getPixel(imageData.data, imageData.width, x - 1, y + 1) +
+        gradientY[7] * getPixel(imageData.data, imageData.width, x, y + 1) +
+        gradientY[8] * getPixel(imageData.data, imageData.width, x + 1, y + 1);
+
+      let magnitude = Math.sqrt(pX * pX + pY * pY);
+
+      pixels.push(magnitude, magnitude, magnitude, 255);
+    }
+  }
+
+  let newImageData = targetCanvasCtx.createImageData(
+    targetCanvas.width,
+    targetCanvas.height
+  );
+
+  newImageData.data.set(new Uint8ClampedArray(pixels));
+
+  console.log(newImageData);
+
+  targetCanvasCtx.putImageData(newImageData, 0, 0);
+}
